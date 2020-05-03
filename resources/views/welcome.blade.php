0019372fea3700e2de6694c0834c71fc711eed87
Arifin N <arifinofficial30@gmail.com>
2020-05-04 02:07:02 +0800

    remove google fonts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        {{-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> --}}
     </head>
     <body>
         <div class="flex-center position-ref full-height">
